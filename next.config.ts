cb0503e47675315838e9177665d0ea060e12eeb7
Marlon Kuzmick <mkuzmick@gmail.com>
2025-01-01 16:04:33 -0500

    Updates:

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["pbs.twimg.com", "files.slack.com",],
+  },
 };
 
 export default nextConfig;
