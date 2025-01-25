c360347b4400981f510f72c9573b6f011743b29e
mk <mk@mac.lan>
2025-01-25 15:19:25 -0500

    Updates: eslint updates

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,15 +1,5 @@
 import { ReactNode } from 'react';
 import MarkdownNoteEditor from '@/components/tools/markdown/MarkdownNoteEditor';
-interface ComponentProps {
-    children: ReactNode;
-}
-const Component = ({children}: ComponentProps) => {
-    return (
-        <div>
-            {children}
-        </div>
-    );
-}
 
 export default function Page() {
     return (
